e787152b60551955596d92abdec4c7bf0e1ff300
ONT549 <propgg2@gmail.com>
2025-07-02 04:37:34 +0900

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { supabase } from './supabaseClient';
-
-console.log('Main JS loaded');
-
-supabase.auth.getUser().then((res) => {
-  console.log('현재 로그인된 사용자:', res.data.user);
-});
--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://dbdnvjbkotlyarsfmtjn.supabase.co';  // ← 실제 프로젝트 URL로 교체
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiZG52amJrb3RseWFyc2ZtdGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5NTUxNTUsImV4cCI6MjA2MjUzMTE1NX0.cQXd9NQQbEQ5tWAeQ2pJWIoGkQjAbVs4tOQAUeDCET0';                    // ← 실제 키로 교체
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
