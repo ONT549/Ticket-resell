f2b09eba68f46fcb86cf143d9549b6fdc9689bbb
ONT549 <propgg2@gmail.com>
2025-07-02 03:11:23 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import './style.css';
+import { supabase } from './supabaseClient';
 
 console.log('Main JS loaded');
+
+supabase.auth.getUser().then((res) => {
+  console.log('현재 로그인된 사용자:', res.data.user);
+});
