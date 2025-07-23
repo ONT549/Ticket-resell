82eb85df8c3608feb71d86247feb683c3fca3f4b
ONT549 <propgg2@gmail.com>
2025-07-23 18:02:25 +0900

    Create stadiumSeats.test.js

---
+++ tests/stadiumSeats.test.js
@@ -0,0 +1,24 @@
+const { updateSeatOptions, stadiumSeats } = require('../stadiumSeats');
+
+describe('updateSeatOptions', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <select id="stadium">
+        <option value="잠실야구장">잠실야구장</option>
+      </select>
+      <select id="seat"></select>
+    `;
+    updateSeatOptions('stadium', 'seat');
+  });
+
+  test('selecting a stadium populates seat options', () => {
+    const stadiumSelect = document.getElementById('stadium');
+    const seatSelect = document.getElementById('seat');
+
+    stadiumSelect.value = '잠실야구장';
+    stadiumSelect.dispatchEvent(new Event('change'));
+
+    const seatTexts = Array.from(seatSelect.querySelectorAll('option')).map(opt => opt.textContent);
+    expect(seatTexts).toEqual(stadiumSeats['잠실야구장']);
+  });
+});
