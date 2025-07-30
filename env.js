169f6f71abc496fe14cd02b18206fe42bc13c0f9
ONT549 <propgg2@gmail.com>
2025-07-31 01:01:43 +0900

    Create env.js

---
+++ env.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL      = 'https://dbdnvjbkotlyarsfmtjn.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiZG52amJrb3RseWFyc2ZtdGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5NTUxNTUsImV4cCI6MjA2MjUzMTE1NX0.cQXd9NQQbEQ5tWAeQ2pJWIoGkQjAbVs4tOQAUeDCET0';
